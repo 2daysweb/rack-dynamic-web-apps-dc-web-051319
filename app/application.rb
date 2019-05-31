603b65d23f29852cc30afa44fef3e132cab84d55
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-31 16:08:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,9 +7,9 @@ class Application
     num3 = Kernel.rand(1..20)
     
     if num1==num2 && num2==num3 
-      write "You Win!"
+      resp.write "You Win!"
     else 
-      write "You Lose"
+      resp.write "You Lose"
     resp.write "Hello, World"
     resp.finish
   end
