5d49d393f3d6d0aed5d63b7cd450cda2db6759a4
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-31 16:08:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     num1 = Kernel.rand(1..20)
+    num2 = Kernel.rand(1..20)
+    num3 = Kernel.rand(1..20)
     
+    if num1==num2 && num2==num3 
+      write "You Win!"
+    else 
+      write "You Lose"
     resp.write "Hello, World"
     resp.finish
   end
